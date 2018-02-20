8fa599e84c47baeae88bc9d2edd1cfb46e433f94
Rishabh Harit <rishabh876@gmail.com>
2018-02-20 23:16:19 +0530

    First Commit

---
+++ app/src/main/java/com/rishabh/goodshows/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.rishabh.goodshows
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home_activity)
+    }
+}
